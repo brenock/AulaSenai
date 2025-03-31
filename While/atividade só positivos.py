11d63ce4df8de75aab2375cd0fff07e431ebb010
Breno Lopes <brenonobrelopes@gmail.com>
2025-03-31 14:50:07 -0300

    Atividade só positivos

---
+++ While/atividade só positivos.py
@@ -0,0 +1,14 @@
+while True:
+ contador = int(input("Digite um numero positivo:")) #10
+
+ if contador < 0:
+     print("O numero deve ser positivo!")
+     continue
+ else:
+     print("O numero é positivo")
+     break
+
+
+
+
+
