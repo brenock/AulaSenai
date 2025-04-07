5c078672bbb08170a0f9fa0516ff3ce7749b0f67
Breno Lopes <brenonobrelopes@gmail.com>
2025-04-07 08:36:46 -0300

    Atividade acessando itens da lista

---
+++ Vetores(listas)/AcessandoItemLista.py
@@ -0,0 +1,10 @@
+numeros = [10,20,30,40,50]
+
+#Acesssando o primeiro número (índice 0)
+primeiro = numeros[0] #10
+
+#Acessando o terceiro número (índice2)
+terceiro = numeros[2] #30
+
+#Acessando o último número (índice -1)
+ultimo = numeros[-1] #50
--- Vetores(listas)/Vetores.py
+++ Vetores(listas)/Vetores.py
@@ -1,3 +1,7 @@
 #Ordem definida: Semelhante a uma fila de espera, os elementos de uma lista python tem uma ordem específica e podem se acessados por sua posição
 listas_de_compras = ["nissan","bmw","chevrolet","ferrari"]
 print(listas_de_compras)
+
+#Atividade primeira lista
+lista = ["morango", "abacaxi","uva","laranja","manga"]
+print(lista)
