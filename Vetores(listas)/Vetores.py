33c5d3702292a7e992000971f1916f6c25ec87b9
Breno Lopes <brenonobrelopes@gmail.com>
2025-04-07 07:52:37 -0300

    Criação do diretorio, listas e vetores

---
+++ Vetores(listas)/Vetores.py
@@ -0,0 +1,3 @@
+#Ordem definida: Semelhante a uma fila de espera, os elementos de uma lista python tem uma ordem específica e podem se acessados por sua posição
+listas_de_compras = ["nissan","bmw","chevrolet","ferrari"]
+print(listas_de_compras)
